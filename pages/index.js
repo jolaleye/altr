0eb548a9b90eab9d5a16e181f075c29c2463e448
jolaleye <j.olaleye.o@gmail.com>
2019-02-12 19:42:24 -0600

    Add poppins font & media queries

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { breakpoints } from '../theme';
 import Head from 'next/head';
 import Header from '../components/Header';
 
@@ -6,6 +7,7 @@ export default () => (
     <Head>
       <title>Altr</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600" rel="stylesheet" />
     </Head>
 
     <div className="root">
@@ -20,6 +22,29 @@ export default () => (
 
         html {
           font-size: 16px;
+          line-height: 1.5;
+          color: hsl(0, 0%, 18%);
+        }
+
+        @media ${breakpoints.md} {
+          html {
+            font-size: 20px;
+            line-height: 1.875;
+          }
+        }
+
+        @media ${breakpoints.lg} {
+          html {
+            font-size: 24px;
+            line-height: 2.25;
+          }
+        }
+
+        @media ${breakpoints.xl} {
+          html {
+            font-size: 38px;
+            line-height: 3.56;
+          }
         }
       `}</style>
     </div>
