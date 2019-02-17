3e60f3632cb5275b476d5fab4a18b6cf63dc0415
jolaleye <j.olaleye.o@gmail.com>
2019-02-17 00:18:22 -0600

    Add useInterval react hook

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 import mime from 'mime';
+import { useEffect, useRef } from 'react';
 
 // prettier-ignore
 const supportedExt = ['jpeg', 'jpg', 'png', 'tiff', 'tif', 'webp', 'mp4', 'mov', 'qt', 'wmv', 'avi', 'mkv', 'webm', 'wav', 'mp3', 'wma'];
@@ -6,4 +7,23 @@ export function validateMIME(type) {
   const ext = mime.getExtension(type);
   if (!/^image|^video|^audio/.test(type) || !supportedExt.includes(ext)) return false;
   return true;
+}
+
+// react interval hook
+export function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  // Set up the interval.
+  useEffect(() => {
+    const tick = () => savedCallback.current();
+    if (delay !== null) {
+      let cycle = setInterval(tick, delay);
+      return () => clearInterval(cycle);
+    }
+  }, [delay]);
 }
