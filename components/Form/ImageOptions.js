bcdaa1181bef285df7f41dc28051de1ff4b87c9f
jolaleye <j.olaleye.o@gmail.com>
2019-02-16 19:53:31 -0600

    Show quality & compression conditionally

--- components/Form/ImageOptions.js
+++ components/Form/ImageOptions.js
@@ -4,10 +4,11 @@ import Select from './Fields/Select';
 import Slider from './Fields/Slider';
 
 export default function ImageOptions() {
-  const [format, setFormat] = useState();
+  const [format, setFormat] = useState('jpg');
   const [width, setWidth] = useState();
   const [height, setHeight] = useState();
   const [quality, setQuality] = useState(90);
+  const [compression, setCompression] = useState(1);
 
   return (
     <div className="options">
@@ -20,12 +21,20 @@ export default function ImageOptions() {
         </Select>
       </div>
       <div className="row">
-        <Number label={'Width'} value={width} onChange={e => setWidth(e.target.value)} min={1} />
-        <Number label={'Height'} value={height} onChange={e => setHeight(e.target.value)} min={1} />
+        <Number label={'Width'} placeholder={'600'} value={width} onChange={e => setWidth(e.target.value)} min={1} />
+        <Number label={'Height'} placeholder={'400'} value={height} onChange={e => setHeight(e.target.value)} min={1} />
       </div>
-      <div className="row">
-        <Slider label={'Quality'} value={quality} onChange={e => setQuality(e.target.value)} min={1} max={100} />
-      </div>
+      {/jpg|tiff|webp/.test(format) && (
+        <div className="row">
+          <Slider label={'Quality'} value={quality} onChange={e => setQuality(e.target.value)} min={1} max={100} />
+        </div>
+      )}
+      {format === 'png' && (
+        <div className="row">
+          {/* prettier-ignore */}
+          <Slider label={'Compression'} value={compression} onChange={e => setCompression(e.target.value)} min={0} max={7} />
+        </div>
+      )}
 
       <style jsx>{`
         .options {
