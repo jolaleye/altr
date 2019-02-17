e9b7e73e7d47fe5fbfc25e7636d6057ac42991b7
jolaleye <j.olaleye.o@gmail.com>
2019-02-16 18:37:19 -0600

    Add text input component

---
+++ components/Form/Fields/TextInput.js
@@ -0,0 +1,41 @@
+export default function TextInput({ label, value, onChange }) {
+  return (
+    <label>
+      {label}
+      <input type="text" value={value} onChange={onChange} />
+
+      <style jsx>{`
+        label {
+          display: flex;
+          flex-flow: column nowrap;
+          align-items: flex-start;
+          width: 42%;
+          font-weight: 600;
+          font-size: 0.8em;
+          color: hsla(0, 0%, 18%, 0.9);
+        }
+
+        input {
+          padding: 0.75em 1em;
+          background-color: hsl(0, 0%, 97%);
+          border-radius: 10px;
+          border: none;
+          font-family: inherit;
+          font-weight: inherit;
+          font-size: 1em;
+          width: 100%;
+          margin-top: 0.5em;
+          color: hsla(0, 0%, 18%, 0.8);
+        }
+
+        input::placeholder {
+          color: hsla(0, 0%, 18%, 0.5);
+        }
+
+        input:focus {
+          outline: none;
+        }
+      `}</style>
+    </label>
+  );
+}
