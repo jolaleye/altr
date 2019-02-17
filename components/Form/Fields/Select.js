948936a403e62a9ae9a1694561b0aadbf8499fec
jolaleye <j.olaleye.o@gmail.com>
2019-02-16 18:37:12 -0600

    Add select component

---
+++ components/Form/Fields/Select.js
@@ -0,0 +1,57 @@
+export default function Select({ children, label, value, onChange }) {
+  return (
+    <label>
+      {label}
+      <div>
+        <select value={value} onChange={onChange}>
+          {children}
+        </select>
+        <i className="uil uil-angle-down" />
+      </div>
+
+      <style jsx>{`
+        label {
+          display: flex;
+          flex-flow: column nowrap;
+          align-items: flex-start;
+          width: 42%;
+          font-weight: 600;
+          font-size: 0.8em;
+          color: hsla(0, 0%, 18%, 0.9);
+        }
+
+        div {
+          display: flex;
+          padding: 0.75em 1em;
+          background-color: hsl(0, 0%, 97%);
+          border-radius: 10px;
+          overflow: hidden;
+          font-weight: 600;
+          font-size: 0.8em;
+          width: 100%;
+          font-size: 1em;
+          color: hsla(0, 0%, 18%, 0.8);
+          text-align: left;
+        }
+
+        div i {
+          font-size: 1.2em;
+        }
+
+        select {
+          font-family: inherit;
+          font-weight: inherit;
+          font-size: inherit;
+          appearance: none;
+          border: none;
+          color: inherit;
+          width: 100%;
+        }
+
+        select:focus {
+          outline: none;
+        }
+      `}</style>
+    </label>
+  );
+}
