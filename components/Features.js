5d92570e013f3e8cadda99652727a5f63acf3efe
jolaleye <j.olaleye.o@gmail.com>
2019-02-18 22:23:52 -0600

    Add background blobs

--- components/Features.js
+++ components/Features.js
@@ -1,32 +1,37 @@
+import { breakpoints } from '../theme';
+
 export default function Features() {
   return (
     <section id="features">
-      <h1>Features</h1>
-      <ul>
-        <li>
-          <i className="uil uil-check" />
-          Convert between image, video, and audio file formats
-        </li>
-        <li>
-          <i className="uil uil-check" />
-          Adjust image quality and compression
-        </li>
-        <li>
-          <i className="uil uil-check" />
-          Import images, videos, and audio by URL
-        </li>
-        <li>
-          <i className="uil uil-check" />
-          Change image and video dimensions
-        </li>
-        <li>
-          <i className="uil uil-check" />
-          Trim video and audio clips
-        </li>
-      </ul>
+      <Background />
+      <div className="wrapper">
+        <h1>Features</h1>
+        <ul>
+          <li>
+            <i className="uil uil-check" />
+            Convert between image, video, and audio file formats
+          </li>
+          <li>
+            <i className="uil uil-check" />
+            Adjust image quality and compression
+          </li>
+          <li>
+            <i className="uil uil-check" />
+            Import images, videos, and audio by URL
+          </li>
+          <li>
+            <i className="uil uil-check" />
+            Change image and video dimensions
+          </li>
+          <li>
+            <i className="uil uil-check" />
+            Trim video and audio clips
+          </li>
+        </ul>
+      </div>
 
       <style jsx>{`
-        section {
+        .wrapper {
           padding: 3.5em 0.75em;
           display: flex;
           flex-flow: column nowrap;
@@ -55,5 +60,120 @@ export default function Features() {
         }
       `}</style>
     </section>
+  );
+}
+
+function Background() {
+  return (
+    <div>
+      <img className="dots" src="/static/img/dots.svg" alt="" />
+      <img className="dots2" src="/static/img/dots2.svg" alt="" />
+      <img className="circle" src="/static/img/circle2.svg" alt="" />
+      <img className="blob" src="/static/img/blob.svg" alt="" />
+
+      <style jsx>{`
+        div {
+          position: relative;
+          z-index: -1;
+        }
+
+        .dots,
+        .dots2,
+        .circle,
+        .blob {
+          position: absolute;
+          transform-origin: center;
+        }
+
+        .dots {
+          width: 5em;
+          top: 0;
+          left: 0;
+          transform: translate(-20%, 20%);
+        }
+
+        .dots2 {
+          width: 7em;
+          top: 0;
+          right: 0;
+          transform: translate(20%, -5%);
+        }
+
+        .circle {
+          width: 5em;
+          top: 0;
+          left: 0;
+          transform: translate(-30%, 575%) rotate(90deg);
+        }
+
+        .blob {
+          width: 8em;
+          top: 0;
+          right: 0;
+          transform: translate(40%, 550%) rotate(-20deg);
+        }
+
+        @media ${breakpoints.sm} {
+          .dots {
+            width: 6em;
+            transform: translate(22%, 15%);
+          }
+
+          .dots2 {
+            width: 8em;
+            transform: translate(18%, 0%);
+          }
+
+          .circle {
+            width: 6em;
+            transform: translate(-20%, 450%) rotate(90deg);
+          }
+
+          .blob {
+            width: 10em;
+            transform: translate(25%, 400%) rotate(-20deg);
+          }
+        }
+
+        @media ${breakpoints.md} {
+          .dots {
+            width: 6em;
+          }
+
+          .dots2 {
+            width: 9em;
+          }
+
+          .circle {
+            width: 9em;
+            transform: translate(-30%, 275%) rotate(90deg);
+          }
+
+          .blob {
+            width: 13em;
+            transform: translate(20%, 300%) rotate(-30deg);
+          }
+        }
+
+        @media ${breakpoints.lg} {
+          .dots {
+            width: 7em;
+          }
+
+          .dots2 {
+            width: 10em;
+          }
+
+          .circle {
+            width: 9em;
+            transform: translate(-30%, 280%) rotate(90deg);
+          }
+
+          .blob {
+            width: 14em;
+          }
+        }
+      `}</style>
+    </div>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,7 @@ import Form from './Form/Form';
 export default function Header() {
   return (
     <header>
+      <Background />
       <Nav />
       <h1>Transform your images, videos, and audio with ease.</h1>
       <Form />
@@ -27,5 +28,95 @@ export default function Header() {
         }
       `}</style>
     </header>
+  );
+}
+
+function Background() {
+  return (
+    <div>
+      <img className="circle" src="/static/img/circle.svg" alt="" />
+      <img className="blob" src="/static/img/blob.svg" alt="" />
+      <img className="blob2" src="/static/img/blob2.svg" alt="" />
+
+      <style jsx>{`
+        div {
+          position: relative;
+          z-index: -1;
+        }
+
+        .circle,
+        .blob,
+        .blob2 {
+          position: absolute;
+          transform-origin: center;
+        }
+
+        .circle {
+          width: 6em;
+          top: 0;
+          left: 0;
+          transform: translate(110%, -65%);
+        }
+
+        .blob {
+          width: 10em;
+          top: 0;
+          left: 0;
+          transform: translate(-45%, 225%) rotate(30deg);
+        }
+
+        .blob2 {
+          width: 9em;
+          top: 0;
+          right: 0;
+          transform: translate(55%, 220%) rotate(100deg);
+        }
+
+        @media ${breakpoints.sm} {
+          .circle {
+            width: 10em;
+            transform: translate(125%, -80%);
+          }
+
+          .blob {
+            width: 12em;
+            transform: translate(-60%, 210%) rotate(100deg);
+          }
+
+          .blob2 {
+            width: 11em;
+            transform: translate(50%, 150%) rotate(50deg);
+          }
+        }
+
+        @media ${breakpoints.md} {
+          .circle {
+            width: 12em;
+          }
+
+          .blob {
+            width: 14em;
+          }
+
+          .blob2 {
+            width: 13em;
+          }
+        }
+
+        @media ${breakpoints.lg} {
+          .circle {
+            width: 14em;
+          }
+
+          .blob {
+            width: 16em;
+          }
+
+          .blob2 {
+            width: 15em;
+          }
+        }
+      `}</style>
+    </div>
   );
 }
